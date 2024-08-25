b58f3a2e2445ab29316172b9e6457791959173fd
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2024-08-25 09:19:24 +0100

    separated tweet functions

--- index.ts
+++ index.ts
@@ -1,36 +1,53 @@
 import { twitterClient } from "./twitterClient";
 import { fetchBestItems, fetchWorstItems } from "./getItems";
 
-const tweetItems = async () => {
-  const bestItems = await fetchBestItems();
-  const WorstItems = await fetchWorstItems();
+const TweetBestItems = async () => {
+  let bestItems = await fetchBestItems();
   let payload;
 
   for (const item of bestItems) {
-    payload = `
-  🔫: ${item.itemname}
-  💵: $${item.price}
-  📈24h change: ${item.daychange}%`;
-    await twitterClient.v2.tweet(payload);
-    await sleep(60000);
-  }
+    if (item.price === 0) {
+      bestItems = await fetchBestItems();
+    }
 
-  payload = ``;
-  for (const item of WorstItems) {
     payload = `
   🔫: ${item.itemname}
   💵: $${item.price}
-  📈24h change: ${item.daychange}%`;
+  📈24h change: ${item.daychange}%
+  #CS2Skins #CS2Trading #CS2Investing`;
     await twitterClient.v2.tweet(payload);
     await sleep(60000);
   }
 };
 
+const TweetWorstItems = async () => {
+  let WorstItems = await fetchWorstItems();
+  let payload;
+
+  for (const item of WorstItems) {
+    if (item.price === 0) {
+      WorstItems = await fetchWorstItems();
+    }
+    payload = `
+  🔫: ${item.itemname}
+  💵: $${item.price}
+  📈24h change: ${item.daychange}%
+  #CS2Skins #CS2Trading #CS2Investing`;
+    await twitterClient.v2.tweet(payload);
+
+    console.log(`skin:${item.itemname}
+      Price: $${item.price}
+      24h change: ${item.daychange}%
+      #CS2Skins #CS2Trading #CS2Investing`);
+    await sleep(60000);
+  }
+};
+
 function sleep(ms: number): Promise<void> {
-  console.log("Esperando 1 minuto");
   return new Promise((resolve) => {
     setTimeout(resolve, ms);
   });
 }
 
-tweetItems();
+TweetBestItems();
+TweetWorstItems();
